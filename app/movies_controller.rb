3a3edf9b942efe79ee29d13c6e689ceba64f5805
Mickae Truman <mtruman@nyit.edu>
2019-05-14 00:18:06 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -63,14 +63,14 @@ end
 def can_find_using_where_clause_and_be_sorted
   # For this test return all movies released after 2002 and ordered by 
   # release date descending
-  Movie.where("release_date > 2002").order(release_date: :desc)
+  movie = Movie.where("release_date > 2002").order(release_date: :desc)
 end
 
 def can_be_found_updated_and_saved
   # Updtate the title "Awesome Flick" to "Even Awesomer Flick", save it, then return it
   Movie.create(title: "Awesome Flick")
-  movie = Movie.find_by(title :"Awesome Flick")
-  movie.update(title :"Even Awesomer Flick")
+  movie = Movie.find_by(title:"Awesome Flick")
+  movie.update(title:"Even Awesomer Flick")
   movie.save
 end
 
@@ -78,7 +78,7 @@ def can_update_using_update_method
   # Update movie title to "Wat, huh?"
   Movie.create(title: "Wat?")
   movie = Movie.find_by(title :"Wat?")
-  movie.update(title :"Wat, huh?")
+  movie.update(title:"Wat, huh?")
 end
 
 def can_update_multiple_items_at_once
